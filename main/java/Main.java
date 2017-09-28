a1e8973038589784e6033e09e014f6f6273212fe
Vladimir Shefer <quameu@gmail.com>
2017-09-28 23:11:33 +0300

    asd
    asd

---
+++ main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello again, Shefer");
+        System.out.println(Integer.MAX_VALUE + "  " + (Integer.MAX_VALUE + Integer.MAX_VALUE) );
+    }
+}
